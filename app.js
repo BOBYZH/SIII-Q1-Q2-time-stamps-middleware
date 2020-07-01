548eccd9d1069a0c71d6ba97baa4e3cfbd1c27ff
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-07-02 01:22:54 +0800

    Finish Q1: time stamps of request

--- app.js
+++ app.js
@@ -2,30 +2,56 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// 設定 view engine
+app.engine('handlebars', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+
+// 設定middleware訊息內容
+function timeStamp (req) {
+  console.log(`${new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' })} | ${req.method} from ${req.originalUrl}`)
+}
+
+// 避免出現'GET from /favicon.ico'
+app.get('/favicon.ico', (req, res) => res.status(204))
+
 // 列出全部 Todo
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  res.render('index')
+  timeStamp(req)
 })
 
 // 新增一筆 Todo 頁面
 app.get('/new', (req, res) => {
   res.send('新增 Todo 頁面')
+  timeStamp(req)
 })
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/:id', (req, res) => {
   res.send('顯示一筆 Todo')
+  timeStamp(req)
 })
 
 // 新增一筆  Todo
 app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
+  timeStamp(req)
 })
 
 app.delete('/:id/delete', (req, res) => {
   res.send('刪除 Todo')
+  timeStamp(req)
 })
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
+  console.log('Link: http://localhost:3000/')
 })
